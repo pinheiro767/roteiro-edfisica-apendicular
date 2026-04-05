dc106463111ec6d2a138b598170ce3dc4eddda71
pinheiro767 <claudiapinheiro767@gmail.com>
2026-04-05 12:46:25 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,27 +1,33 @@
-const CACHE_NAME = 'roteiro-esqueletico-cache-v1';
-const ASSETS = [
-  './',
-  './index.html',
-  './manifest.webmanifest',
-  './icon-192.png',
-  './icon-512.png'
-];
-self.addEventListener('install', event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
-  self.skipWaiting();
-});
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))
-  );
-  self.clients.claim();
-});
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(cached => cached || fetch(event.request).then(resp => {
-      const copy = resp.clone();
-      caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
-      return resp;
-    }).catch(() => caches.match('./index.html')))
-  );
-});
+const CACHE="roteiro-esqueleto-v1"
+
+self.addEventListener("install",e=>{
+
+e.waitUntil(
+
+caches.open(CACHE).then(cache=>{
+
+return cache.addAll([
+"/",
+"/index.html",
+"/style.css"
+])
+
+})
+
+)
+
+})
+
+self.addEventListener("fetch",e=>{
+
+e.respondWith(
+
+caches.match(e.request).then(res=>{
+
+return res||fetch(e.request)
+
+})
+
+)
+
+})
